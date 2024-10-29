d9f73cd47c8946e33b28d41d3ce4881fcbd91716
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-10-29 21:09:00 +0000

    Create template and recipe_list.html

--- tastyyum/urls.py
+++ tastyyum/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from foodblog.views import my_foodblog
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('foodblog/', my_foodblog, name='foodblog'),
+    path('', include('foodblog.urls')),
 ]
