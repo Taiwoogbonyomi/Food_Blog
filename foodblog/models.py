ab04ff3bc85e93a5873185a9aa57867f581ce904
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-10-29 20:42:10 +0000

    Add comment model

--- foodblog/models.py
+++ foodblog/models.py
@@ -23,5 +23,13 @@ class Recipe(models.Model):
     
     def __str__(self):
         return self.title
- 
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.recipe}'
 
