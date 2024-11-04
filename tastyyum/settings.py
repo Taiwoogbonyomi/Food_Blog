cd90db16ba6d4322420cefef16caae11e8bae92d
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-11-04 02:39:18 +0000

    Install crispy forms and fix bugs in forms

--- tastyyum/settings.py
+++ tastyyum/settings.py
@@ -42,11 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'foodblog',
 ]
 
 SITE_ID = 1
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
