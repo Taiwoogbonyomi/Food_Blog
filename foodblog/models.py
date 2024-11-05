e8cd428d320f24db16d1b4061108dbcba50d8d63
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-11-05 03:57:02 +0000

    Add field updated at to comment model

--- foodblog/models.py
+++ foodblog/models.py
@@ -46,6 +46,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Comment by {self.author} on {self.recipe}'
+        return f"{self.author.username} - {self.content[:20]}"
